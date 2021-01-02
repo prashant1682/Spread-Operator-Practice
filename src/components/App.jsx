9a4becd1e0d2d31be277361a493b5a3edadf1166
prashant1682 <prashant009@gmail.com>
2021-01-02 12:38:36 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // Creating an constant to save the value in the input text and using the state
+  const [inputText, setInputtext] = useState("");
+  //Creating an items array to render the list in the bottom
+  const [items, setItems] = useState([]);
+
+  // on update on the value in the text box , the value will be saved in newValue constant
+  function handleOnChange(event) {
+    const newValue = event.target.value;
+    setInputtext(newValue);
+  }
+
+  function handleOnclick() {
+    setItems(function (prevValue) {
+      return [inputText, ...prevValue];
+    });
+    setInputtext("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleOnChange} type="text" value={inputText} />
+        <button onClick={handleOnclick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map(function (todoItems) {
+            return <li>{todoItems}</li>;
+          })}
         </ul>
       </div>
     </div>
